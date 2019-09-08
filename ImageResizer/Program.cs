9e1842d3c96108c9fb7e97d4391915b6410a2119
Enigman1988 <pibohong@gmail.com>
2019-09-08 13:59:52 +0800

    Finish version

--- ImageResizer/Program.cs
+++ ImageResizer/Program.cs
@@ -9,21 +9,61 @@ namespace ImageResizer
 {
     class Program
     {
+        static string sourcePath;
+        static string destinationPath;
+        static long originSpendingTime = 0;
+        static long newSpendingTime = 0;
+        static double resizeRatio;
+
         static void Main(string[] args)
         {
-            string sourcePath = Path.Combine(Environment.CurrentDirectory, "images");
-            string destinationPath = Path.Combine(Environment.CurrentDirectory, "output"); ;
+            sourcePath = Path.Combine(Environment.CurrentDirectory, "images");
+            destinationPath = Path.Combine(Environment.CurrentDirectory, "output");
+            resizeRatio = 2.0;
 
             ImageProcess imageProcess = new ImageProcess();
 
             imageProcess.Clean(destinationPath);
+            originSpendingTime = ExecuteResizeAndCalculateSpendingTime(new Action<string, string, double>(imageProcess.ResizeImages));
 
+            imageProcess.Clean(destinationPath);
+            newSpendingTime = ExecuteResizeAndCalculateSpendingTime(new Action<string, string, double>(imageProcess.ResizeImagesAsync));
+
+            ShowEfficenty(originSpendingTime, newSpendingTime);
+
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// 計算效率
+        /// </summary>
+        /// <param name="oriTime">原本耗時</param>
+        /// <param name="newTime">改良後耗時</param>
+        static void ShowEfficenty(long oriTime, long newTime)
+        {
+            Console.WriteLine($"同步縮放花費時間: {oriTime} ms");
+            Console.WriteLine($"非同步縮放花費時間: {newTime} ms");
+
+            if (newTime > 0)
+            {
+                var eff = ((double)oriTime / (double)newTime - 1) * 100;
+                Console.WriteLine($"非同步縮放效率提升了 {eff.ToString("f2")}%");
+            }
+        }
+
+        /// <summary>
+        /// 執行縮放並計算執行時間
+        /// </summary>
+        /// <param name="action">縮放方法</param>
+        /// <returns></returns>
+        static long ExecuteResizeAndCalculateSpendingTime(Action<string, string, double> action)
+        {
             Stopwatch sw = new Stopwatch();
             sw.Start();
-            imageProcess.ResizeImages(sourcePath, destinationPath, 2.0);
+            action(sourcePath, destinationPath, resizeRatio);
             sw.Stop();
-
-            Console.WriteLine($"花費時間: {sw.ElapsedMilliseconds} ms");
+            return sw.ElapsedMilliseconds;
         }
     }
 }
